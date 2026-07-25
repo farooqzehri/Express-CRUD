4ce6fd2f102d3c5bbadb64f5340cd18c269f754c
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-07-26 01:25:57 +0500

    form inputs

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,24 @@
-fetch('https://express-crud-swart.vercel.app/api/v1/todo')
-.then(res => res.json())
-.then(res => {
-    console.log(res.todos);
-    
+
+const api = 'https://express-crud-swart.vercel.app/api/v1/todo'
+async function addTodo() {
+    const title = document.querySelector('#title')
+    const description = document.querySelector('#description')
+
+
+fetch(api , {
+    method: 'GET',
+    headers: {
+        "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+        title,
+        description
+    })
 })
+
+const data = await response.json()
+console.log(data);
+
+}
 
 // all ok error fixed on vercel
