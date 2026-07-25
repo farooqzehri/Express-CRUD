7dca7bcc4b17d6ff80332b649e227d3f3bfdd493
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-07-26 01:47:49 +0500

    get items fix..

--- frontend/app.js
+++ frontend/app.js
@@ -1,13 +1,22 @@
 
 const API = 'https://express-crud-swart.vercel.app/api/v1/todo';
+const render = document.querySelector('.render')
 
 const getAllTodo = () => {
     fetch(API)
-    .then(res => res.json())
-    .then(res => {
-        console.log(res.todos);
-        
-    })
+        .then(res => res.json())
+        .then(res => {
+            const allItems = res.todos
+            console.log(allItems);
+            allItems.map((item, index) => {
+                render.innerHTML += `<li>
+                <h1>${item.title}</h1>
+                <h2>${item.description}</h2>
+                
+                </li>`
+            })
+
+        })
 }
 getAllTodo()
 
