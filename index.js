636666809f9e696b40fabdf8dfb09dda9846e874
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-07-28 04:26:09 +0500

    all set

--- index.js
+++ index.js
@@ -3,19 +3,24 @@ import connectDB from './src/db/index.js'
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import path from "path";
+import { fileURLToPath } from "url";
 
 // muje kall nAHI AAJJ ISSUE TEEK KERNA HAI 
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const port = 3000
 
 app.use(express.json());
 app.use(cors())
+app.use(express.static(path.join(__dirname, "frontend")));
 
 dotenv.config();
-app.get('/' , (req , res) => {
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
 app.use('/api/v1' , todoRoute )
 
 
